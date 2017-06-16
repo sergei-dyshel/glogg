cbef92a4c02ecb59d495b19481f59e79a8d994f0
Sergei Dyshel <qyron.private@gmail.com>
2017-06-16 21:04:30 +0300

    Restore tab bar default size

--- src/tabbedcrawlerwidget.cpp
+++ src/tabbedcrawlerwidget.cpp
@@ -42,14 +42,10 @@ TabbedCrawlerWidget::TabbedCrawlerWidget() : QTabWidget(),
               subcontrol-position: left;\
              }" );
 #else
-    // On GTK style, it looks better with a smaller font
     myTabBar_.setStyleSheet(
             "QTabBar::tab {"
-            " height: 20px; "
-            " font-size: 9pt; "
             "} "
             "QTabBar::close-button {\
-              height: 6px; width: 6px;\
               subcontrol-origin: padding;\
               subcontrol-position: left;\
              }" );
