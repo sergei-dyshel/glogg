1fbbfac52252d4ed1cb42e89500eaa2b4cb36bfd
Sergei Dyshel <qyron.private@gmail.com>
2019-03-20 18:51:57 +0200

    + add yaml config

--- src/exception.h
+++ src/exception.h
@@ -1,4 +1,21 @@
-// TODO: add header
+/*
+ * Copyright (C) 2018-2019 Sergei Dyshel and other contributors
+ *
+ * This file is part of glogg.
+ *
+ * glogg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * glogg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with glogg.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
@@ -25,6 +42,7 @@
 class Exception : public std::exception {
   public:
     Exception(const LogContext &context) : stream_(context) {}
+    virtual ~Exception() {}
     const char *what() const noexcept override;
     const QString &message() const { return stream_.message(); }
 
